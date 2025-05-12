a5be3a590bc64b892eeb8883f96a4f245098a0df
Dmytro <dimka670020040@gmail.com>
2025-05-13 00:21:22 +0300

    tabsBar

---
+++ src/app/(tabs)/newsPlus.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const NewsPlusScreen = () => {
+  return (
+    <View>
+      <Text>Screen</Text>
+    </View>
+  );
+};
+
+export default NewsPlusScreen;
+
+const styles = StyleSheet.create({});
---
+++ src/app/_layout.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+import React from 'react';
+import { Slot, Stack } from 'expo-router';
+
+const RootLayout = () => {
+  return <Slot />;
+};
+
+export default RootLayout;
